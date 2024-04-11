a9a6ba4c4e5e5f70f92ae73267521b2e7fd10ac3
bitsonymousm <bitsonymous@gmail.com>
2024-04-11 16:17:24 +0530

    started new project where ill use api

---
+++ pr03currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>currency app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ pr03currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+
+
+function useCurrencyInfo(currency){
+    
+}
